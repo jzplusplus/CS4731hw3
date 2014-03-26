b18d06fdddcddc03e56cf1b4ad2bee006e02ef35
Jay Zuerndorfer <jzplusplus@gmail.com>
2014-03-26 17:01:09 -0400

    Added hill sections back, and made a penalty for repeated sections

--- MarioLevelComp2011_cs4731/src/dk/itu/mario/level/MyLevel.java
+++ MarioLevelComp2011_cs4731/src/dk/itu/mario/level/MyLevel.java
@@ -5,11 +5,9 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.Random;
 
-import dk.itu.mario.MarioInterface.Constraints;
 import dk.itu.mario.MarioInterface.GamePlay;
 import dk.itu.mario.MarioInterface.LevelInterface;
 import dk.itu.mario.engine.sprites.SpriteTemplate;
-import dk.itu.mario.engine.sprites.Enemy;
 
 
 public class MyLevel extends Level{
@@ -61,7 +59,7 @@ public class MyLevel extends Level{
 //		System.out.println("Aimless jumps" + playerMetrics.aimlessJumps);
 		// hardcoded for now
 		//model = decideModel();
-		model = KILLER;
+		model = COLLECTOR;
 		
 		random = new Random(seed);
 		
@@ -107,7 +105,7 @@ public class MyLevel extends Level{
 
 		//create the start location
 		int length = 0;
-		length += buildStraight(0, width, true);
+		length += buildStraight(0, width);
 		
 		for(LevelSection ls : levelSections)
 		{
@@ -173,263 +171,11 @@ public class MyLevel extends Level{
 	}
 
 
-	private int buildJump(int xo, int maxLength)
-	{	gaps++;
-	//jl: jump length
-	//js: the number of blocks that are available at either side for free
-	int js = random.nextInt(4) + 2;
-	int jl = random.nextInt(2) + 2;
-	int length = js * 2 + jl;
-
-	boolean hasStairs = random.nextInt(3) == 0;
-
-	int floor = height - 1 - random.nextInt(4);
-	//run from the start x position, for the whole length
-	for (int x = xo; x < xo + length; x++)
-	{
-		if (x < xo + js || x > xo + length - js - 1)
-		{
-			//run for all y's since we need to paint blocks upward
-			for (int y = 0; y < height; y++)
-			{	//paint ground up until the floor
-				if (y >= floor)
-				{
-					setBlock(x, y, GROUND);
-				}
-				//if it is above ground, start making stairs of rocks
-				else if (hasStairs)
-				{	//LEFT SIDE
-					if (x < xo + js)
-					{ //we need to max it out and level because it wont
-						//paint ground correctly unless two bricks are side by side
-						if (y >= floor - (x - xo) + 1)
-						{
-							setBlock(x, y, ROCK);
-						}
-					}
-					else
-					{ //RIGHT SIDE
-						if (y >= floor - ((xo + length) - x) + 2)
-						{
-							setBlock(x, y, ROCK);
-						}
-					}
-				}
-			}
-		}
-	}
-
-	return length;
-	}
-
-	private int buildCannons(int xo, int maxLength)
-	{
-		int length = random.nextInt(10) + 2;
-		if (length > maxLength) length = maxLength;
-
-		int floor = height - 1 - random.nextInt(4);
-		int xCannon = xo + 1 + random.nextInt(4);
-		for (int x = xo; x < xo + length; x++)
-		{
-			if (x > xCannon)
-			{
-				xCannon += 2 + random.nextInt(4);
-			}
-			if (xCannon == xo + length - 1) xCannon += 10;
-			int cannonHeight = floor - random.nextInt(4) - 1;
-
-			for (int y = 0; y < height; y++)
-			{
-				if (y >= floor)
-				{
-					setBlock(x, y, GROUND);
-				}
-				else
-				{
-					if (x == xCannon && y >= cannonHeight)
-					{
-						if (y == cannonHeight)
-						{
-							setBlock(x, y, (byte) (14 + 0 * 16));
-						}
-						else if (y == cannonHeight + 1)
-						{
-							setBlock(x, y, (byte) (14 + 1 * 16));
-						}
-						else
-						{
-							setBlock(x, y, (byte) (14 + 2 * 16));
-						}
-					}
-				}
-			}
-		}
-
-		return length;
-	}
-
-	private int buildHillStraight(int xo, int maxLength)
-	{
-		int length = random.nextInt(10) + 10;
-		if (length > maxLength) length = maxLength;
-
-		int floor = height - 1 - random.nextInt(4);
-		for (int x = xo; x < xo + length; x++)
-		{
-			for (int y = 0; y < height; y++)
-			{
-				if (y >= floor)
-				{
-					setBlock(x, y, GROUND);
-				}
-			}
-		}
-
-		addEnemyLine(xo + 1, xo + length - 1, floor - 1);
-
-		int h = floor;
-
-		boolean keepGoing = true;
-
-		boolean[] occupied = new boolean[length];
-		while (keepGoing)
-		{
-			h = h - 2 - random.nextInt(3);
-
-			if (h <= 0)
-			{
-				keepGoing = false;
-			}
-			else
-			{
-				int l = random.nextInt(5) + 3;
-				int xxo = random.nextInt(length - l - 2) + xo + 1;
-
-				if (occupied[xxo - xo] || occupied[xxo - xo + l] || occupied[xxo - xo - 1] || occupied[xxo - xo + l + 1])
-				{
-					keepGoing = false;
-				}
-				else
-				{
-					occupied[xxo - xo] = true;
-					occupied[xxo - xo + l] = true;
-					addEnemyLine(xxo, xxo + l, h - 1);
-					if (random.nextInt(4) == 0)
-					{
-						decorate(xxo - 1, xxo + l + 1, h);
-						keepGoing = false;
-					}
-					for (int x = xxo; x < xxo + l; x++)
-					{
-						for (int y = h; y < floor; y++)
-						{
-							int xx = 5;
-							if (x == xxo) xx = 4;
-							if (x == xxo + l - 1) xx = 6;
-							int yy = 9;
-							if (y == h) yy = 8;
-
-							if (getBlock(x, y) == 0)
-							{
-								setBlock(x, y, (byte) (xx + yy * 16));
-							}
-							else
-							{
-								if (getBlock(x, y) == HILL_TOP_LEFT) setBlock(x, y, HILL_TOP_LEFT_IN);
-								if (getBlock(x, y) == HILL_TOP_RIGHT) setBlock(x, y, HILL_TOP_RIGHT_IN);
-							}
-						}
-					}
-				}
-			}
-		}
-
-		return length;
-	}
-
-	private void addEnemyLine(int x0, int x1, int y)
-	{
-		for (int x = x0; x < x1; x++)
-		{
-			if (random.nextInt(35) < difficulty + 1)
-			{
-				int type = random.nextInt(4);
-
-				if (difficulty < 1)
-				{
-					type = Enemy.ENEMY_GOOMBA;
-				}
-				else if (difficulty < 3)
-				{
-					type = random.nextInt(3);
-				}
-
-				setSpriteTemplate(x, y, new SpriteTemplate(type, random.nextInt(35) < difficulty));
-				ENEMIES++;
-			}
-		}
-	}
-
-	private int buildTubes(int xo, int maxLength)
-	{
-		int length = random.nextInt(10) + 5;
-		if (length > maxLength) length = maxLength;
-
-		int floor = height - 1 - random.nextInt(4);
-		int xTube = xo + 1 + random.nextInt(4);
-		int tubeHeight = floor - random.nextInt(2) - 2;
-		for (int x = xo; x < xo + length; x++)
-		{
-			if (x > xTube + 1)
-			{
-				xTube += 3 + random.nextInt(4);
-				tubeHeight = floor - random.nextInt(2) - 2;
-			}
-			if (xTube >= xo + length - 2) xTube += 10;
-
-			if (x == xTube && random.nextInt(11) < difficulty + 1)
-			{
-				setSpriteTemplate(x, tubeHeight, new SpriteTemplate(Enemy.ENEMY_FLOWER, false));
-				ENEMIES++;
-			}
-
-			for (int y = 0; y < height; y++)
-			{
-				if (y >= floor)
-				{
-					setBlock(x, y,GROUND);
-
-				}
-				else
-				{
-					if ((x == xTube || x == xTube + 1) && y >= tubeHeight)
-					{
-						int xPic = 10 + x - xTube;
-
-						if (y == tubeHeight)
-						{
-							//tube top
-							setBlock(x, y, (byte) (xPic + 0 * 16));
-						}
-						else
-						{
-							//tube side
-							setBlock(x, y, (byte) (xPic + 1 * 16));
-						}
-					}
-				}
-			}
-		}
-
-		return length;
-	}
-
-	private int buildStraight(int xo, int maxLength, boolean safe)
+	private int buildStraight(int xo, int maxLength)
 	{
 		int length = random.nextInt(10) + 2;
 
-		if (safe)
-			length = 10 + random.nextInt(5);
+		length = 10 + random.nextInt(5);
 
 		if (length > maxLength)
 			length = maxLength;
@@ -448,86 +194,7 @@ public class MyLevel extends Level{
 			}
 		}
 
-		if (!safe)
-		{
-			if (length > 5)
-			{
-				decorate(xo, xo + length, floor);
-			}
-		}
-
 		return length;
-	}
-
-	private void decorate(int xStart, int xLength, int floor)
-	{
-		//if its at the very top, just return
-				if (floor < 1)
-					return;
-
-		//        boolean coins = random.nextInt(3) == 0;
-		boolean rocks = true;
-
-		//add an enemy line above the box
-		addEnemyLine(xStart + 1, xLength - 1, floor - 1);
-
-		int s = random.nextInt(4);
-		int e = random.nextInt(4);
-
-		if (floor - 2 > 0){
-			if ((xLength - 1 - e) - (xStart + 1 + s) > 1){
-				for(int x = xStart + 1 + s; x < xLength - 1 - e; x++){
-					setBlock(x, floor - 2, COIN);
-					COINS++;
-				}
-			}
-		}
-
-		s = random.nextInt(4);
-		e = random.nextInt(4);
-
-		//this fills the set of blocks and the hidden objects inside them
-		if (floor - 4 > 0)
-		{
-			if ((xLength - 1 - e) - (xStart + 1 + s) > 2)
-			{
-				for (int x = xStart + 1 + s; x < xLength - 1 - e; x++)
-				{
-					if (rocks)
-					{
-						if (x != xStart + 1 && x != xLength - 2 && random.nextInt(3) == 0)
-						{
-							if (random.nextInt(4) == 0)
-							{
-								setBlock(x, floor - 4, BLOCK_POWERUP);
-								BLOCKS_POWER++;
-							}
-							else
-							{	//the fills a block with a hidden coin
-								setBlock(x, floor - 4, BLOCK_COIN);
-							BLOCKS_COINS++;
-							}
-						}
-						else if (random.nextInt(4) == 0)
-						{
-							if (random.nextInt(4) == 0)
-							{
-								setBlock(x, floor - 4, (byte) (2 + 1 * 16));
-							}
-							else
-							{
-								setBlock(x, floor - 4, (byte) (1 + 1 * 16));
-							}
-						}
-						else
-						{
-							setBlock(x, floor - 4, BLOCK_EMPTY);
-							BLOCKS_EMPTY++;
-						}
-					}
-				}
-			}
-		}
 	}
 
 	private void fixWalls()
@@ -733,7 +400,7 @@ public class MyLevel extends Level{
 	
 	public LevelSectionList generateSections(int width)
 	{
-		final int POPULATION_SIZE = 100;
+		final int POPULATION_SIZE = 200;
 		final int GENERATIONS = 100;
 		final int NUMBER_TO_KEEP = 25;
 		
@@ -803,8 +470,8 @@ public class MyLevel extends Level{
 	
 	public LevelSection randomSection(int maxLength)
 	{
-		int id = random.nextInt(3);
-		//TODO add more sections options
+		int id = random.nextInt(4);
+		
 		switch(id) {
 		case 0:
 			return new AdvancedJumpSection();
@@ -812,10 +479,11 @@ public class MyLevel extends Level{
 			return new StraightSection(maxLength);
 		case 2:
 			return new CannonSection(maxLength);
-//		case 3:
-//			return new TubeSection(maxLength);
+		case 3:
+			return new HillSection(maxLength);
 //		case 4:
-//			return new HillSection(maxLength);
+//			return new TubeSection(maxLength);
+		
 		default:
 			return null;
 		}
@@ -836,6 +504,23 @@ public class MyLevel extends Level{
 		
 		public void add(LevelSection ls)
 		{
+			//penalize two of the same section in a row
+			if(sections.size() > 0 && ls.getId() == sections.get(sections.size()-1).getId())
+			{
+				fitness -= 2;
+				
+				if(sections.size() > 1 && ls.getId() == sections.get(sections.size()-2).getId())
+				{
+					fitness -= 10;
+					
+					if(sections.size() > 2 && ls.getId() == sections.get(sections.size()-3).getId())
+					{
+						fitness -= 20;
+					}
+				}
+			}
+			
+			
 			sections.add(ls);
 			length += ls.getLength();
 			fitness += ls.fitness();
@@ -921,7 +606,7 @@ public class MyLevel extends Level{
 			switch(model)
 			{
 			case PRECISION:
-				return jl - js - vDiff + (hasStairs ? 3 : 0);
+				return 2 * (jl - js - vDiff + (hasStairs ? 3 : 0));
 				
 			case COLLECTOR:
 				return 0;
@@ -930,7 +615,7 @@ public class MyLevel extends Level{
 				return 0;
 				
 			case HARDCORE:
-				return jl - js - vDiff + (hasStairs ? 3 : 0);
+				return 2 * (jl - js - vDiff + (hasStairs ? 3 : 0));
 				
 			default:
 				
@@ -1113,7 +798,7 @@ public class MyLevel extends Level{
 			switch(model)
 			{
 			case PRECISION:
-				return -length + 2;
+				return -length/2 + 2;
 				
 			case COLLECTOR:
 				return coins;
@@ -1142,13 +827,15 @@ public class MyLevel extends Level{
 				}
 			}
 
-			addEnemyLine(xo + 1, xo + length - 1, floor - 1);
+			//addEnemyLine(xo + 1, xo + length - 1, floor - 1);
+			if(enemies > 0) addEnemies(xo + 1, xo + length - 1, floor, enemies/4);
 
 			int h = floor;
 
 			boolean keepGoing = true;
 
 			boolean[] occupied = new boolean[length];
+
 			while (keepGoing)
 			{
 				h = h - 2 - random.nextInt(3);
@@ -1170,10 +857,11 @@ public class MyLevel extends Level{
 					{
 						occupied[xxo - xo] = true;
 						occupied[xxo - xo + l] = true;
-						addEnemyLine(xxo, xxo + l, h - 1);
+
+						if(enemies > 0) addEnemies(xxo, xxo + l, h - 1, enemies/4);
+						
 						if (random.nextInt(4) == 0)
 						{
-							decorate(xxo - 1, xxo + l + 1, h);
 							keepGoing = false;
 						}
 						for (int x = xxo; x < xxo + l; x++)
@@ -1198,6 +886,10 @@ public class MyLevel extends Level{
 							}
 						}
 					}
+					
+					
+					if(h < 5) keepGoing = false;
+					if(!keepGoing && coins > 0)  addCoins(xxo - 1, xxo + l + 1, h, coins);
 				}
 			}
 
@@ -1205,91 +897,91 @@ public class MyLevel extends Level{
 		}
 	}
 	
-	//tubes
-	public class TubeSection extends LevelSection{
-		
-		private int id = TUBES_SECTION;
-		
-		private int length;
-		
-		public TubeSection(int maxLength)
-		{
-			length = random.nextInt(10) + 5;
-			if (length > maxLength) length = maxLength;
-		}
-		
-		public int getId() {
-			return id;
-		}
-		
-		public int getLength()
-		{
-			return length;
-		}
-		
-		public double fitness()
-		{
-			switch(model)
-			{
-			case PRECISION:
-				return -length + 2;
-			default:
-				
-				break;
-			}
-			return 0.0;
-		}
-		
-		public int build(int xo, int maxLength) {
-			int floor = height - 1 - random.nextInt(4);
-			int xTube = xo + 1 + random.nextInt(4);
-			int tubeHeight = floor - random.nextInt(2) - 2;
-			for (int x = xo; x < xo + length; x++)
-			{
-				if (x > xTube + 1)
-				{
-					xTube += 3 + random.nextInt(4);
-					tubeHeight = floor - random.nextInt(2) - 2;
-				}
-				if (xTube >= xo + length - 2) xTube += 10;
-
-				if (x == xTube && random.nextInt(11) < difficulty + 1)
-				{
-					setSpriteTemplate(x, tubeHeight, new SpriteTemplate(Enemy.ENEMY_FLOWER, false));
-					ENEMIES++;
-				}
-
-				for (int y = 0; y < height; y++)
-				{
-					if (y >= floor)
-					{
-						setBlock(x, y,GROUND);
-
-					}
-					else
-					{
-						if ((x == xTube || x == xTube + 1) && y >= tubeHeight)
-						{
-							int xPic = 10 + x - xTube;
-
-							if (y == tubeHeight)
-							{
-								//tube top
-								setBlock(x, y, (byte) (xPic + 0 * 16));
-							}
-							else
-							{
-								//tube side
-								setBlock(x, y, (byte) (xPic + 1 * 16));
-							}
-						}
-					}
-				}
-			}
-			
-			return length;
-		}
-	}
+//	//tubes
+//	public class TubeSection extends LevelSection{
+//		
+//		private int id = TUBES_SECTION;
+//		
+//		private int length;
+//		
+//		public TubeSection(int maxLength)
+//		{
+//			length = random.nextInt(10) + 5;
+//			if (length > maxLength) length = maxLength;
+//		}
+//		
+//		public int getId() {
+//			return id;
+//		}
+//		
+//		public int getLength()
+//		{
+//			return length;
+//		}
+//		
+//		public double fitness()
+//		{
+//			switch(model)
+//			{
+//			case PRECISION:
+//				return -length + 2;
+//			default:
+//				
+//				break;
+//			}
+//			return 0.0;
+//		}
+//		
+//		public int build(int xo, int maxLength) {
+//			int floor = height - 1 - random.nextInt(4);
+//			int xTube = xo + 1 + random.nextInt(4);
+//			int tubeHeight = floor - random.nextInt(2) - 2;
+//			for (int x = xo; x < xo + length; x++)
+//			{
+//				if (x > xTube + 1)
+//				{
+//					xTube += 3 + random.nextInt(4);
+//					tubeHeight = floor - random.nextInt(2) - 2;
+//				}
+//				if (xTube >= xo + length - 2) xTube += 10;
+//
+//				if (x == xTube && random.nextInt(11) < difficulty + 1)
+//				{
+//					setSpriteTemplate(x, tubeHeight, new SpriteTemplate(Enemy.ENEMY_FLOWER, false));
+//					ENEMIES++;
+//				}
+//
+//				for (int y = 0; y < height; y++)
+//				{
+//					if (y >= floor)
+//					{
+//						setBlock(x, y,GROUND);
+//
+//					}
+//					else
+//					{
+//						if ((x == xTube || x == xTube + 1) && y >= tubeHeight)
+//						{
+//							int xPic = 10 + x - xTube;
+//
+//							if (y == tubeHeight)
+//							{
+//								//tube top
+//								setBlock(x, y, (byte) (xPic + 0 * 16));
+//							}
+//							else
+//							{
+//								//tube side
+//								setBlock(x, y, (byte) (xPic + 1 * 16));
+//							}
+//						}
+//					}
+//				}
+//			}
+//			
+//			return length;
+//		}
+//	}
 	
 	//cannons
 	public class CannonSection extends LevelSection{
@@ -1402,15 +1094,17 @@ public class MyLevel extends Level{
 	{
 		if (y - 2 <= 0) return;
 		
+		if(number > endX - startX) number = endX - startX;
+		
 		for (int i=0; i < number; i++)
 		{
 			int x = random.nextInt(endX - startX) + startX;
-			while(this.getBlock(x, y - 2) == COIN)
+			while(getBlock(x, y - 1) == COIN)
 			{
 				x = random.nextInt(endX - startX) + startX;
 			}
-				
-			setBlock(x, y - 2, COIN);
+			
+			setBlock(x, y - 1, COIN);
 			COINS++;
 		}
 	}
